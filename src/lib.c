4077d0ce2912df5f0fed989c5de079bdd5e84231
Dan Nisenson <dan.nisenson@twinny.es>
2024-11-02 10:22:34 +0100

    Move logic to lib.c

--- src/lib.c
+++ src/lib.c
@@ -1,3 +1,49 @@
-int add(int a, int b) {
-  return a + b;
+#include <stdio.h>
+#include <string.h>
+
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+#define FIZZBUZZ "FizzBuzz"
+
+void intToFizzBuzz(int n, char *buff);
+
+void fizz_buzz(char *buffer, int limit)
+{
+    for (int i = 1; i <= limit; i++)
+    {
+        char buff[10] = {};
+        intToFizzBuzz(i, buff);
+        strcat(buffer, buff);
+
+        if (i != limit)
+        {
+            strcat(buffer, ", ");
+        }
+        else
+        {
+            strcat(buffer, ".");
+        }
+    }
+}
+
+void intToFizzBuzz(int n, char *buff)
+{
+    if (n % 3 == 0 && n % 5 == 0)
+    {
+        strcpy(buff, FIZZBUZZ);
+    }
+    else if (n % 3 == 0)
+    {
+        strcpy(buff, FIZZ);
+    }
+    else if (n % 5 == 0)
+    {
+        strcpy(buff, BUZZ);
+    }
+    else
+    {
+        char tmp[3] = {};
+        sprintf(tmp, "%d", n);
+        strcpy(buff, tmp);
+    }
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lib.c"
 
 #define LIMIT 30
@@ -10,43 +8,8 @@ void fizz_buzz(char *buffer, int limit);
 int main()
 {
     char buffer[1000] = {};
-
     fizz_buzz(buffer, LIMIT);
 
     printf("Result:\n%s", buffer);
     return 0;
-}
-
-void fizz_buzz(char *buffer, int limit)
-{
-    for (int i = 1; i <= limit; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            strcat(buffer, "FizBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            strcat(buffer, "Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            strcat(buffer, "Buzz");
-        }
-        else
-        {
-            char tmp[3] = {};
-            sprintf(tmp, "%d", i);
-            strcat(buffer, tmp);
-        }
-
-        if (i != limit)
-        {
-            strcat(buffer, ", ");
-        }
-        else
-        {
-            strcat(buffer, ".");
-        }
-    }
 }
